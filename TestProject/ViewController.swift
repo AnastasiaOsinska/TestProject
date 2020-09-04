744b1c5c8053e253eb3190ff3db184a2b024a02d
Anastasia Osinska <60783750+AnastasiaOsinska@users.noreply.github.com>
2020-09-04 16:00:50 +0300

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Anastasiya Osinskaya on 9/4/20.
+//  Copyright © 2020 Anastasiya Osinskaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
